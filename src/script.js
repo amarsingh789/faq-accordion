dad8b9e040fbbd1974572e473d9ecbe299916ba0
Amar Singh <amar298976@gmail.com>
2025-09-18 10:03:49 +0530

    Initial commit -FAQ accordion project

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(q => {
+    q.addEventListener("click", ()=> {
+        const answer = q.nextElementSibling;
+        const icon = q.querySelector("img");
+
+        //check agar already open hai
+        const isOpen = !answer.classList.contains("hidden");
+
+        //sab answer close karo & icon reset karo
+        document.querySelectorAll(".answer").forEach((ans) => ans.classList.add("hidden"));
+        document.querySelectorAll(".question img").forEach((icon) => {
+            icon.src = "/assets/images/icon-plus.svg"
+        });
+        //agar already open nhi tha to isko open karo
+        if(!isOpen){
+            answer.classList.remove("hidden");
+            icon.src = "/assets/images/icon-minus.svg";
+        }
+    });
+})
